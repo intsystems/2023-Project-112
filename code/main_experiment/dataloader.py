a702039ee33a30fa3a511e4774c54cd1070255b1
Nikita Kiselev <nekitbrain@yandex.ru>
2023-10-10 03:43:44 +0300

    Many changes in the code (added scripts, removed unnecessary code)

---
+++ code/main_experiment/dataloader.py
@@ -0,0 +1,97 @@
+import numpy as np
+import os
+import torch
+import torchvision.transforms as transforms
+import cv2
+from PIL import Image
+import nibabel as nib
+
+
+class FramesLoader:
+
+    """Load and prepare vidoe for models."""
+
+    def __init__(self) -> None:
+        """Initialize paths for video and frames."""
+        self.fig_path = os.path.join(os.path.dirname(
+            os.path.dirname(os.getcwd())), "figures")
+        self.video_path = os.path.join(os.path.dirname(
+            os.path.dirname(os.getcwd())), "src", "Film stimulus.mp4")
+
+    def _video_to_frames(self):
+        """Import video from video_path and divide it into the frames."""
+        videocap = cv2.VideoCapture(self.video_path)
+        success, frame = videocap.read()
+        count = 1
+        while success:
+            cv2.imwrite(os.path.join(os.path.dirname(os.path.dirname(
+                os.getcwd())), "src", "frames", f"frame_{count}.jpg"), frame)
+            success, frame = videocap.read()
+            count += 1
+
+    def _frames_to_tensors(self):
+        """Takes frames from video and transform them into the tensors."""
+        preprocess = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+        ])
+
+        for i in range(1, 9751):
+            frame_path = os.path.join(os.path.dirname(os.path.dirname(
+                os.getcwd())), "src", "frames", f"frame_{i}.jpg")
+            frame = Image.open(frame_path)
+            frame_tensor = preprocess(frame)
+            frame_tensor = frame_tensor.unsqueeze(0)
+            yield frame_tensor
+
+    def _tensors_to_vectors(self):
+        """Takes frames tensors and transform them into the vectors using ResNet152."""
+        model = torch.hub.load('pytorch/vision:v0.6.0',
+                               'resnet152', pretrained=True, verbose=False)
+
+        for param in model.parameters():
+            param.requires_grad = False
+
+        model.fc = torch.nn.Identity()
+
+        for frame_tensor in self._frames_to_tensors():
+            # передача картинки в модель и получение выходных данных
+            with torch.no_grad():
+                output = model(frame_tensor)
+            # преобразование выходных данных в вектор
+            vector = output.numpy().flatten()
+            yield vector
+
+    def get_vector_list(self, load=True):
+        """Returns vector_list which consists of frames embeddings."""
+        if load == True:
+            vector_list = np.load(os.path.join(os.path.dirname(
+                os.path.dirname(os.getcwd())), "src", "vector_list.npy"))
+        else:
+            vector_list = [vector for vector in self._tensors_to_vectors()]
+            np.save("vector_list", vector_list)
+        return vector_list
+
+
+class Sub:
+
+    """Responsible for the subject and contains information about his data."""
+
+    subs_with_fmri = ['04', '07', '08', '09', '11', '13', '14', '15', '16', '18',
+                      '22', '24', '27', '28', '29', '31', '35', '41', '43', '44',
+                      '45', '46', '47', '51', '52', '53', '55', '56', '60', '62']
+
+    def __init__(self, number):
+        if not number in Sub.subs_with_fmri:
+            raise ValueError(f"У {number} испытуемого отсутствуют снимки фМРТ")
+        else:
+            self.number = number
+        self.path = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "src", "ds003688-download", f"sub-{self.number}",
+                                 "ses-mri3t", "func", f"sub-{self.number}_ses-mri3t_task-film_run-1_bold.nii.gz")
+        self.scan = nib.load(self.path)
+        self.data = self.scan.get_fdata()
+        self.tensor = torch.tensor(self.data)
+        self.tensor_np = self.tensor.numpy()
---
+++ code/main_experiment/models.py
@@ -0,0 +1,177 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import nibabel as nib
+import torch
+import torchvision.transforms as transforms
+import cv2
+from PIL import Image
+from sklearn.linear_model import LinearRegression
+from scipy.ndimage import gaussian_filter
+from scipy.signal import convolve
+import scipy.signal as signal
+import random
+from sklearn.metrics import r2_score
+import seaborn as sns
+import pickle
+import utils
+
+
+class Preprocessor:
+
+    """Preprocesses data for the model. Splits the sample into train and test using the dt parameter."""
+
+    def __init__(self, vector_list, sub, dt, coef, train_size=0.7):
+        self.vector_list = vector_list
+        self.sub = sub
+        self.dt = dt
+        self.coef = coef
+        self.train_size = train_size
+
+        self.nu = 25  # частота видео
+        self.mu = 641. / 390.  # частота снимков фМРТ
+        self.d1 = self.sub.tensor.shape[0]  # размерности снимка фМРТ до сжатия
+        self.d2 = self.sub.tensor.shape[1]
+        self.d3 = self.sub.tensor.shape[2]
+        self.d4 = self.sub.tensor.shape[3]
+        self.d = 2048  # длина вектора признакового описания изображения
+        self.N = 641 - int(self.mu * self.dt)  # N - количество снимков фМРТ
+
+        self.train, self.test = self.get_train_test()
+
+    def get_train_test(self):
+        pairs = [(int(i * self.nu / self.mu), int(self.mu * self.dt + i))
+                 for i in range(self.N)]  # (номер изображения, номер снимка)
+
+        if (self.coef > 1):  # сжатие снимка фМРТ
+            maxpool = torch.nn.MaxPool3d(
+                kernel_size=self.coef, stride=self.coef)
+            input_tensor = self.sub.tensor.permute(3, 0, 1, 2)
+            output_tensor = maxpool(input_tensor).permute(1, 2, 3, 0)
+            self.sub._tensor = output_tensor
+        else:
+            self.sub._tensor = self.sub.tensor
+
+        self._d1 = self.sub._tensor.shape[0]
+        self._d2 = self.sub._tensor.shape[1]
+        self._d3 = self.sub._tensor.shape[2]
+        self._d4 = self.sub._tensor.shape[3]
+
+        scans_list = [self.sub._tensor[:, :, :, i]
+                      for i in range(self.d4)]  # список тензоров снимков фМРТ
+        # список снимков фМРТ, развернутых в векторы
+        voxels = [scan.reshape(self._d1 * self._d2 * self._d3).numpy()
+                  for scan in scans_list]
+        data = [(self.vector_list[n], voxels[k])
+                for n, k in pairs]  # (изображение, снимок)
+
+        # train, test
+        l = int(self.train_size * self.d4)  # размер обучающей выборки
+        train, test = data[:l], data[l:]
+
+        train = [(pair[0], utils.preprocess(pair[1])) for pair in train]
+        test = [(pair[0], utils.preprocess(pair[1])) for pair in test]
+
+        return train, test
+
+
+class LinearModel(Preprocessor):
+
+    """Model that predict next fMRI scan."""
+
+    def __init__(self, vector_list, sub, dt, coef, alpha, train_size=0.7):
+        super().__init__(vector_list, sub, dt, coef, train_size)
+        self.delta = False
+        self.alpha = alpha
+        self.X_train, self.Y_train, self.X_test, self.Y_test = self.get_XY()
+
+    def get_XY(self):
+        X_train = np.array([pair[0] for pair in self.train])
+        Y_train = np.array([pair[1] for pair in self.train]).T
+        X_test = np.array([pair[0] for pair in self.test])
+        Y_test = np.array([pair[1] for pair in self.test]).T
+        return X_train, Y_train, X_test, Y_test
+
+    def fit(self):
+        W = []  # матрица весов модели
+
+        if (self.alpha > 0):
+            A = np.linalg.inv(self.X_train.T @ self.X_train + self.alpha *
+                              np.identity(self.X_train.shape[1])) @ self.X_train.T
+        else:
+            A = np.linalg.pinv(self.X_train)
+
+        for i in range(self._d1 * self._d2 * self._d3):
+            Y_train_vector = self.Y_train[i]
+            w = A @ Y_train_vector
+            W.append(w)
+
+        self.W = np.array(W)  # w будут строками
+
+    def predict(self):
+        self.Y_train_predicted = self.W @ self.X_train.T
+        self.Y_test_predicted = self.W @ self.X_test.T
+
+    def evaluate(self):
+        self.MSE_train = utils.MSE(self.Y_train_predicted - self.Y_train)
+        self.MSE_test = utils.MSE(self.Y_test_predicted - self.Y_test)
+
+class LinearDeltaModel(Preprocessor):
+
+    """Model that predict the difference between current and next fMRI scans."""
+
+    def __init__(self, vector_list, sub, dt, coef, alpha, train_size=0.7):
+        super().__init__(vector_list, sub, dt, coef, train_size)
+        self.delta = True
+        self.alpha = alpha
+        self.X_train, self.Y_train, self.deltaY_train, self.X_test, self.Y_test, self.deltaY_test = self.get_XY()
+
+    def get_XY(self):
+        delta_train = [(self.train[n][0], self.train[n][1] -
+                        self.train[n-1][1]) for n in range(1, len(self.train))]
+        delta_test = [(self.test[n][0], self.test[n][1] - self.test[n-1][1])
+                      for n in range(1, len(self.test))]
+        Y_train = np.array([pair[1] for pair in self.train]).T
+        Y_test = np.array([pair[1] for pair in self.test]).T
+        X_train = np.array([pair[0] for pair in delta_train])
+        deltaY_train = np.array([pair[1] for pair in delta_train]).T
+        X_test = np.array([pair[0] for pair in delta_test])
+        deltaY_test = np.array([pair[1] for pair in delta_test]).T
+        return X_train, Y_train, deltaY_train, X_test, Y_test, deltaY_test
+
+    def fit(self):
+        W = []  # матрица весов модели
+
+        if (self.alpha > 0):
+            A = np.linalg.inv(self.X_train.T @ self.X_train + self.alpha *
+                              np.identity(self.X_train.shape[1])) @ self.X_train.T
+        else:
+            A = np.linalg.pinv(self.X_train)
+
+        for i in range(self._d1 * self._d2 * self._d3):
+            deltaY_train_vector = self.deltaY_train[i]
+            w = A @ deltaY_train_vector
+            W.append(w)
+
+        self.W = np.array(W)  # w будут строками
+
+    def predict(self):
+        self.deltaY_train_predicted = self.W @ self.X_train.T
+        self.deltaY_test_predicted = self.W @ self.X_test.T
+        self.Y_train_predicted = np.delete(
+            self.Y_train, -1, 1) + self.deltaY_train_predicted
+        self.Y_test_predicted = np.delete(
+            self.Y_test, -1, 1) + self.deltaY_test_predicted
+
+    def evaluate(self, Y_test_predicted=None):
+        if Y_test_predicted is None:
+            self.MSE_train = utils.MSE(self.Y_train_predicted - np.delete(self.Y_train, 0, 1))
+            self.MSE_test = utils.MSE(self.Y_test_predicted - np.delete(self.Y_test, 0, 1))
+        else:
+            return utils.MSE(Y_test_predicted - np.delete(self.Y_test, 0, 1))
+
+    def repredict(self, W):
+        deltaY_test_predicted = W @ self.X_test.T
+        Y_test_predicted = np.delete(
+            self.Y_test, -1, 1) + deltaY_test_predicted
+        return Y_test_predicted
