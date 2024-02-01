e6114d10218eca374fd2abc6ef1109c733c3b263
Nikita Kiselev <nekitbrain@yandex.ru>
2024-02-01 18:14:18 +0300

    preprocess

--- code/dataloader.py
+++ code/dataloader.py
@@ -1,10 +1,12 @@
 import numpy as np
 import os
 import torch
+import torchvision
 import torchvision.transforms as transforms
 import cv2
 from PIL import Image
 import nibabel as nib
+from tqdm import tqdm
 
 
 class FramesLoader:
@@ -16,47 +18,52 @@ class FramesLoader:
         self.fig_path = os.path.join(os.path.dirname(os.getcwd()), "figures")
         self.video_path = os.path.join(os.path.dirname(
             os.getcwd()), "src", "Film stimulus.mp4")
+        self.videocap = cv2.VideoCapture(self.video_path)
+        self.preprocess = torchvision.models.ResNet152_Weights.IMAGENET1K_V2.transforms()
 
     def _video_to_frames(self):
         """Import video from video_path and divide it into the frames."""
-        videocap = cv2.VideoCapture(self.video_path)
-        success, frame = videocap.read()
+        success, frame = self.videocap.read()
         count = 1
         while success:
             cv2.imwrite(os.path.join(os.path.dirname(os.getcwd()),
                         "src", "frames", f"frame_{count}.jpg"), frame)
-            success, frame = videocap.read()
+            success, frame = self.videocap.read()
             count += 1
 
     def _frames_to_tensors(self):
         """Takes frames from video and transform them into the tensors."""
-        preprocess = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
-        ])
+
+        #preprocess = transforms.Compose([
+        #    transforms.Resize(256),
+        #    transforms.CenterCrop(224),
+        #    transforms.ToTensor(),
+        #    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+        #                         std=[0.229, 0.224, 0.225])
+        #])
 
         for i in range(1, 9751):
             frame_path = os.path.join(os.path.dirname(
                 os.getcwd()), "src", "frames", f"frame_{i}.jpg")
             frame = Image.open(frame_path)
-            frame_tensor = preprocess(frame)
+            frame_tensor = self.preprocess(frame)
             frame_tensor = frame_tensor.unsqueeze(0)
             yield frame_tensor
 
     def _tensors_to_vectors(self):
         """Takes frames tensors and transform them into the vectors using ResNet152."""
-        model = torch.hub.load('pytorch/vision:v0.6.0',
-                               'resnet152', pretrained=True, verbose=False)
 
-        for param in model.parameters():
-            param.requires_grad = False
+        #model = torch.hub.load('pytorch/vision:v0.6.0',
+        #                       'resnet152', pretrained=True, verbose=False)
+        #
+        #for param in model.parameters():
+        #    param.requires_grad = False
+        #
+        #model.fc = torch.nn.Identity()
 
-        model.fc = torch.nn.Identity()
+        model = torchvision.models.resnet152(weights='IMAGENET1K_V2')
 
-        for frame_tensor in self._frames_to_tensors():
+        for frame_tensor in tqdm(self._frames_to_tensors()):
             # передача картинки в модель и получение выходных данных
             with torch.no_grad():
                 output = model(frame_tensor)
@@ -64,14 +71,16 @@ class FramesLoader:
             vector = output.numpy().flatten()
             yield vector
 
-    def get_vector_list(self, load=True):
+    def get_vector_list(self, load=True, new=False):
         """Returns vector_list which consists of frames embeddings."""
         if load == True:
+            filename = "vector_list.npy" if new == False else "vector_list_new.npy"
             vector_list = np.load(os.path.join(
-                os.path.dirname(os.getcwd()), "src", "vector_list.npy"))
+                os.path.dirname(os.getcwd()), "src", filename))
         else:
+            filename = "vector_list" if new == False else "vector_list_new"
             vector_list = [vector for vector in self._tensors_to_vectors()]
-            np.save("vector_list", vector_list)
+            np.save(filename, vector_list)
         return vector_list
 
 
